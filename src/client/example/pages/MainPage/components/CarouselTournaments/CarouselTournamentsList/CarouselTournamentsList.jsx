99f1f4ae48366d1650939db4fac4f771a83f03e9
Vadim Bevziuk <vadw1984@gmail.com>
2020-11-29 13:48:37 +0200

    finish carousel

--- src/client/example/pages/MainPage/components/CarouselTournaments/CarouselTournamentsList/CarouselTournamentsList.jsx
+++ src/client/example/pages/MainPage/components/CarouselTournaments/CarouselTournamentsList/CarouselTournamentsList.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { carouselStyle, styleCarouselDescription, styleCarouselHeader } from './style';
-import { TournamentsData } from '../TournamentsData';
+import {
+  carouselStyle,
+  styleCarouselDescription,
+  styleCarouselHeader,
+} from './style';
+import { TournamentsData } from '../../../../../../../app/store/TournamentsData';
 
-const CarouselTournamentsList = TournamentsData.filter((el)=>el.status==='announce').map((elem)=>{
-    return (
-        <div>
-            <div style={carouselStyle(elem.url_for_banner)}>
-                <h3 style={styleCarouselHeader}>{elem.name}</h3>
-                <p style={styleCarouselDescription}>{elem.description}</p>
-                <button type='button'>SIgn up</button>
-            </div>
-        </div>
-    )
-})
+const CarouselTournamentsList = TournamentsData.filter(
+  el => el.status === 'announce',
+).map(elem => {
+  return (
+    <div>
+      <div style={carouselStyle(elem.url_for_banner)}>
+        <h3 style={styleCarouselHeader}>{elem.name}</h3>
+        <p style={styleCarouselDescription}>{elem.description}</p>
+        <button type="button">SIgn up</button>
+      </div>
+    </div>
+  );
+});
 
-export default CarouselTournamentsList
+export default CarouselTournamentsList;
