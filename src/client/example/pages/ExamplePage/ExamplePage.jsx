28ea2cecba26c659586c7bea53b45796df4955ce
Vadim Bevziuk <vadw1984@gmail.com>
2020-11-29 13:33:25 +0200

    clear branch

--- src/client/example/pages/ExamplePage/ExamplePage.jsx
+++ src/client/example/pages/ExamplePage/ExamplePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import MainPage from '../MainPage';
-import Tournaments from '../Tournaments';
 
 export const ExamplePage = () => {
   return (
-  <>
-    <MainPage/>
-    <Tournaments/>
-  </>
-  ) 
+    <>
+      <MainPage />
+    </>
+  );
 };
