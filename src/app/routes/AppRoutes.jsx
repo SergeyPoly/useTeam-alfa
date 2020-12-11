daf052fbb9b6c436b9e278ab2359bff79fe6698c
Vadim Bevziuk <vadw1984@gmail.com>
2020-12-11 17:56:24 +0200

    main page 111220

--- src/app/routes/AppRoutes.jsx
+++ src/app/routes/AppRoutes.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ExamplePage } from '../../client/example/pages/ExamplePage';
+import { mainPageStyle } from '../../client/Main/pages/MainPage/style';
 
 export const AppRoutes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <ExamplePage />
+        <MainPage />
       </Route>
-      <Route exact path="/foo">
-        <p>BAR</p>
-      </Route>
+      
     </Switch>
   );
 };
---
+++ src/client/Main/pages/MainPage/pageProps;.js
@@ -0,0 +1,16 @@
+import banner1Bg from "../../../../assets/images/banners/bg_banner1.png";
+import banner2Bg from "../../../../assets/images/banners/bg_banner2.png";
+
+export const pageProps = {
+    sectionBanner1: {
+        bg: banner1Bg,
+        title="Why team better then solo play?",
+        text="Create your own team or join to your friends team"
+    },
+    sectionBanner2: {
+        bg: banner2Bg,
+        title="Join useTeam Right now",
+        text="More than 1000 tournaments every month. Check your skills!"
+    },
+    
+}
