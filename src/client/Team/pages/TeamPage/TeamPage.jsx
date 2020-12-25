91960bba13b22be3d834134d48b5d7f64eca90b6
AndriySydorchenko <AndriySydorchenko@gmail.com>
2020-12-25 22:24:26 +0200

    create shared components table and create block tournamentHistory, Teammates, OverviewTeam

--- src/client/Team/pages/TeamPage/TeamPage.jsx
+++ src/client/Team/pages/TeamPage/TeamPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import FormCreateTeam from '../../components/FormCreateTeam';
 import style from './TeamPage.module.scss';
+import OverviewTeam from '../../components/OverviewTeam';
+import { overviewTeamProps } from '../../components/OverviewTeam/overviewTeamProps';
+import Teammates from '../../components/Teammates';
+import { teammatesListProps } from '../../components/Teammates/teammatesListProps';
+import TournamentHistory from '../../components/TornamentHistory';
+import { tournamentHistoryProps } from '../../components/TornamentHistory/tournamentHistoryProps';
 
 const TeamPage = () => {
   return (
     <div className={style.teamPage}>
       <h2 className={style.title}>Team</h2>
       <FormCreateTeam />
+      <OverviewTeam {...overviewTeamProps} />
+      <Teammates {...teammatesListProps} />
+      <TournamentHistory {...tournamentHistoryProps} />
     </div>
   );
 };
