88e368fdff43ffbe83a002ac0f5e180d8ac5a4b8
Oleh <olegzadorohnzyy@gmail.com>
2021-02-22 08:25:06 +0200

    features widget

---
+++ src/client/Partnership/components/FeatureList/FeatureList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import FeatureItem from "./components/FeatureItem/FeatureItem";
+import style from "./FeatureList.module.scss"
+
+const FeatureList = () => {
+    const featureProps = [
+        {
+            name: "daxak",
+            text: "You wanna make an omelet, you gotta break some eggs.",
+            image: "https://ps.w.org/tomparisde-twitchtv-widget/assets/icon-256x256.png",
+            link: "twitch.tv/dota2mc_ru"
+        },
+        {
+            name: "dota2mc_ru",
+            text: "Охотимся за  рэмпейджами и транслируем турниры.",
+            image: "https://ps.w.org/tomparisde-twitchtv-widget/assets/icon-256x256.png",
+            link: "twitch.tv/dota2mc_ru"
+        },
+    ]
+
+    const featureList = featureProps.map(item => <FeatureItem props = {item}/>)
+    return(
+        <div className={style.featureList}>
+            {featureList}
+        </div>
+    )
+}
+
+export default FeatureList;
--- src/client/Partnership/pages/PartnershipPage/PartnershipPage.jsx
+++ src/client/Partnership/pages/PartnershipPage/PartnershipPage.jsx
@@ -5,19 +5,14 @@ import Sidebar from '../../../../shared/containers/Sidebar';
 import PartnershipMainContent from "../../components/PartnershipMainContent";
 import Heading from "../../../../shared/components/Heading";
 import partnerProfileProps from '../../partnerProfileProps'
+import FeatureList from "../../components/FeatureList";
 
 const PartnershipPage = () => {
 
     const sidebarData = [
         {
             headingText: '5x5 Tournaments',
-            Component: ''
-        },
-    ];
-    const sidebarDataBottom = [
-        {
-            headingText: '1x1 Tournaments',
-            Component: ''
+            Component: < div />
         },
     ];
 
@@ -26,11 +21,11 @@ const PartnershipPage = () => {
             <div className={`${styles.tournaments_page__content} container`}>
                 <div className={'column-3'}>
                     <Sidebar sidebarData={sidebarData}/>
-                    <Sidebar sidebarData={sidebarDataBottom}/>
                 </div>
                 <div className={'column-9'}>
                     <Heading type={'page'} text='Partnership'/>
                     <PartnershipMainContent partnerProfileProps={partnerProfileProps}/>
+                    <FeatureList />
                 </div>
             </div>
         </div>
