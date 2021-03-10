54f88406488eead88d042fbe041ed2d6c45b06b8
Sergey Polischuk <spoly82@gmail.com>
2021-03-11 01:36:12 +0200

    fixed bugs

--- src/client/pages/Partnership/partnerProfileProps.js
+++ src/client/pages/Partnership/partnerProfileProps.js
@@ -5,6 +5,6 @@ const partnerProfileProps = {
     matches : 1245,
     events: 246,
     src: "https://static-sl.insales.ru/images/collections/1/5120/9589760/photo_2020-01-24_15-28-06.jpg",
-    srcSmall: "https://avatars.mds.yandex.net/get-zen_doc/101122/pub_5e2a9796028d6800b09b8cc2_5e2aa8abb477bf00b1cc2daf/scale_1200"};
+    srcSmall: "https://images.gameru.net/image/direct/ad9efc5bda99440.jpg"};
 
 export default partnerProfileProps;
