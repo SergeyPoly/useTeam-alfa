5708caffc0d0db63f9c6f6bb5e07e150db169e66
Sergey Polischuk <spoly82@gmail.com>
2021-03-12 17:05:05 +0200

    Updates

---
+++ precache-manifest.05f44881a5381571ac3e67ceca19bffc.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4607bb43e271bd3007486ebc5aecaac9",
+    "url": "/useTeam-alfa/index.html"
+  },
+  {
+    "revision": "6a23216a390d4c5b8aea",
+    "url": "/useTeam-alfa/static/css/main.80ee2415.chunk.css"
+  },
+  {
+    "revision": "48a7c74aff6654841ea6",
+    "url": "/useTeam-alfa/static/js/2.bff514ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/useTeam-alfa/static/js/2.bff514ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0fa01d6a7730370a90",
+    "url": "/useTeam-alfa/static/js/3.5ee60bb2.chunk.js"
+  },
+  {
+    "revision": "6a23216a390d4c5b8aea",
+    "url": "/useTeam-alfa/static/js/main.0f89dd1b.chunk.js"
+  },
+  {
+    "revision": "af852096d752e8208681",
+    "url": "/useTeam-alfa/static/js/runtime-main.b5b570ed.js"
+  },
+  {
+    "revision": "64c9f467a695168d56aecccd1a9f2ca5",
+    "url": "/useTeam-alfa/static/media/PlayerAvatar.64c9f467.png"
+  },
+  {
+    "revision": "205c5d7fb97c0825209da55284ff55c3",
+    "url": "/useTeam-alfa/static/media/bg_banner1.205c5d7f.png"
+  },
+  {
+    "revision": "4d45eb1d621d6eac6d27a5460d74a287",
+    "url": "/useTeam-alfa/static/media/bg_banner2.4d45eb1d.png"
+  },
+  {
+    "revision": "6f0ecb1c25622c55673507c5dc8e80b9",
+    "url": "/useTeam-alfa/static/media/charge.6f0ecb1c.svg"
+  },
+  {
+    "revision": "64c9f467a695168d56aecccd1a9f2ca5",
+    "url": "/useTeam-alfa/static/media/defaultAvatar.64c9f467.png"
+  },
+  {
+    "revision": "b1d74d0bc16bfcc2f549afbfefeefc5e",
+    "url": "/useTeam-alfa/static/media/dollar.b1d74d0b.svg"
+  },
+  {
+    "revision": "17d6b08d2dd1ef112a1179c4883b9097",
+    "url": "/useTeam-alfa/static/media/lightening.17d6b08d.svg"
+  },
+  {
+    "revision": "62ba74b14ba01530f79814fe2e03e440",
+    "url": "/useTeam-alfa/static/media/player_avatar_normal1.62ba74b1.png"
+  },
+  {
+    "revision": "aa2b8b1c4b0296bde53af7bf93e910e4",
+    "url": "/useTeam-alfa/static/media/subpartner_2.aa2b8b1c.png"
+  },
+  {
+    "revision": "b1ea7259b469405673bec510ef6466bc",
+    "url": "/useTeam-alfa/static/media/subpartner_3.b1ea7259.png"
+  },
+  {
+    "revision": "75042f2de174609b1e91509a77dab409",
+    "url": "/useTeam-alfa/static/media/tournament-avatar.75042f2d.jpg"
+  }
+]);
